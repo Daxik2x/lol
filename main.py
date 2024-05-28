26146868692afb66d95f64f18b9e9a57304f3d7c
Max <iamsowa@proton.me>
2024-05-29 01:36:59 +0300

    Добил до финалки, набросил стилей и пасхалку

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session 
+import random
 import psycopg2
 
 app = Flask(__name__)
@@ -10,6 +12,22 @@ pg = psycopg2.connect("""
     port=5432
 """)
 
+
+# Настройки для сессии
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Список фраз для отображения
+phrases = [
+    "Когда я ем — я глух и нем. Когда я пью — я гораздо коммуникабельней!",
+    "Смерть — это защитная реакция организма на нездоровый образ жизни.",
+    "Как тут попадёшь в рай, если пять из семи смертных грехов — это мои хобби?",
+    "Сила нужна, чтобы наносить удары. Выносливость — чтобы держать удары. Ловкость — чтобы избегать ударов. А интеллект... Ну, вообще-то за него и бьют.",
+    "Ученье — свет, а неученье опасно для здоровья."
+]
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -20,6 +38,9 @@ def index():
 @app.route('/<country_code>')
 def get_country_info(country_code):
     country_code = country_code.upper()  # Приводим код страны к верхнему регистру
+    session['requests'] = session.get('requests', 0) + 1  # Увеличиваем счетчик запросов
+    print("Requests count:", session['requests'])  
+
     cursor = pg.cursor()
     
     # Запрос для получения названия страны и количества городов
@@ -31,10 +52,15 @@ def get_country_info(country_code):
         GROUP BY c.name;
     """, (country_code,))
     
+    # Добавляем случайную фразу, если нужно
+    random_phrase = None
+    if session['requests'] % 3 == 0:
+        random_phrase = random.choice(phrases)
+
     result = cursor.fetchone()
     
     if not result:
-        return "Country not found", 404
+        return render_template('country_not_found.html', random_phrase=random_phrase), 404
 
     country_name, city_count = result
 
@@ -49,7 +75,7 @@ def get_country_info(country_code):
 
     cities = cursor.fetchall()
 
-    return render_template('country_info.html', country_name=country_name, city_count=city_count, cities=cities)
+    return render_template('country_info.html', country_name=country_name, city_count=city_count, cities=cities, random_phrase=random_phrase)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
